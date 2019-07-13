f095269bb71992c030834eeb6d5a24efe1a49573
EliColmenero <52494848+EliColmenero@users.noreply.github.com>
2019-07-13 11:30:58 -0300

    modifique cuatro.

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,28 @@
 function mostrar()
 {
+var numeroUno;
+    var numeroDos;
+    var mensaje;
 
+    numeroUno=prompt("Ingrese el primer valor");
+    numeroDos=prompt("Ingrese el segundo valor");
+    if(numeroUno==numeroDos)
+    {
+        mensaje=(numeroUno+""+numeroDos);
+    } else {
+        numeroUno=parseInt(numeroUno);
+        numeroDos=parseInt(numeroDos);
+        if(numeroUno>numeroDos)
+        {
+            mensaje=numeroUno/numeroDos;
+        } else {
+            mensaje=numeroUno+numeroDos;
+        }
+            if((numeroUno+numeroDos)<50)
+            {
+                mensaje="La suma es "+(numeroUno+numeroDos)+" y es menor a 50";
+            } 
+    } 
+alert(mensaje);
+    }
 }
