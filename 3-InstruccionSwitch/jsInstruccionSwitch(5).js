c7c1d70e60611caf5ce8a77b6a692534dc63af40
EliColmenero <52494848+EliColmenero@users.noreply.github.com>
2019-07-13 12:02:33 -0300

    modifique switch 5

--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -5,7 +5,23 @@ var laHora = document.getElementById('hora').value;
 
 //alert (laHora);
 	
-	
+	switch(laHora){
+        case '7': {
+        }
+        case '8': {
+        }
+        case '9': {
+        }
+        case '10': {
+        }
+        case '11': {
+            alert("Es de mañana");
+            break;
+        }
+    }
+    if(laHora>11 || laHora<7) {
+        alert("No es de mañana");
+    }
 
 
 
