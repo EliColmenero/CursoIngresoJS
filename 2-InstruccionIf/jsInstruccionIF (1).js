b88e5a60aad9303a42ae38035e51e4f2dd5819ab
eli <eli@gmail.com>
2019-07-16 19:13:30 -0300

    modifique para practicar

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -2,6 +2,13 @@ function mostrar()
 {
 //tomo la edad  
 
-alert("MODIFICO PARA GIT");
+var edad;
+
+    edad=prompt("Ingrese su edad");
+    document.getElementById('edad').value=edad;
+
+    if(edad==15){
+        alert("Niña bonita");
+    }
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -1,7 +1,15 @@
 function mostrar()
 {
 //tomo la edad  
+var edad;
+var estadoCivil;
 
+edad=document.getElementById('edad').value;
+document.getElementById('estadoCivil').value=estadoCivil;
+
+if(edad>=18 && estadoCivil=="Soltero"){
+    alert("Es oltero y no es menor");
+}
 	
 
 
