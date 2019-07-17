003cd96b0716cb9de2f7d381bc962d8dab64538d
alumno <alumno@utnfra.com.ar>
2019-07-17 09:30:02 -0300

    Modificado iluminacion

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,21 @@
 function mostrar()
 {
+    var numeroUno;
+    var numeroDos;
+
+    numeroUno=prompt("Ingrese el primer valor");
+    numeroDos=prompt("Ingrese el segundo valor");
+
+        if(numeroUno==numeroDos){
+            resultado=numeroUno+numeroDos;
+        } 
+        else
+        {
+            if(numeroUno>numeroDos){
+                alert("El resultado es "+numeroUno/numeroDos);
+            }
+        }
+
+    alert(resultado);
 
 }
