f486da16c4aff2cb0512823ca7cd93f0c1da1711
alumno <alumno@utnfra.com.ar>
2019-07-17 11:38:57 -0300

    Ejercicios While

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,7 +1,15 @@
 function mostrar()
 {
 
-var clave = prompt("ingrese el número clave.");
+var clave 
+
+
+
+while(clave!="utn750"){   //valido siempre el error
+ 
+    clave=prompt("ingrese el número clave."); //para que, cuando entre al bucle porque puso la clave mal, pueda volver a validar la nueva clave inrgesada
+
+}
 
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (5).js
+++ 5-InstruccionWhile/jsIteraciones (5).js
@@ -1,9 +1,13 @@
 function mostrar()
 {
 
-var sexo = prompt("ingrese f ó m .");
+var sexo;
+sexo=prompt("ingrese f ó m .");
+sexo=sexo.toLowerCase();
 
-
+while(sexo!="f" && sexo!="m"){
+    sexo=prompt("ingrese f ó m .");
+}
 
 document.getElementById('Sexo').value=sexo;
 
