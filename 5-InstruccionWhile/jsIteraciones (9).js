7d009ba9014822cff09fa994e4cfe1966d1a22b8
alumno <alumno@utnfra.com.ar>
2019-07-18 10:44:07 -0300

    Modificacion ejercicio while maximo y minimo

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,17 +2,43 @@ function mostrar()
 {
 
 	var contador=0;
-	// declarar variables
-	
+	var maximo=0;
+	var minimo=0;
+	var numero;
 	var respuesta='si';
 
-	while(respuesta!='no')
+while(respuesta!='no')
+{
+	contador ++;
+	numero=prompt("Ingrese un número");
+	numero=parseInt(numero);
+
+	while(isNaN(numero))
 	{
-		
-	
+		numero=prompt("Ingresre un número válido");
+		numero=parseInt(numero);
 	}
 
+	if(contador==1)
+	{
+		minimo=numero;
+		maximo=numero;
+	}
+	if(numero>maximo)
+	{
+		maximo=numero;
+	} 
+	else
+	{
+		if(numero<minimo)
+		{
+			minimo=numero;
+		}
+	}
 
-
+	document.getElementById('maximo').value=maximo;
+	document.getElementById('minimo').value=minimo;	
+	respuesta=prompt("Desea seguir? ");
+}
 
 }//FIN DE LA FUNCIÓN
