85683028b0050833625a67002dcb7662c9ca7725
eli <kkk>
2019-07-20 12:58:44 -0300

    modificacion while 10

--- 5-InstruccionWhile/jsIteraciones (10).js
+++ 5-InstruccionWhile/jsIteraciones (10).js
@@ -1,17 +1,21 @@
 function mostrar()
 {
-
 	var contador=0;
 	var promedioNegativo;
 	var promedioPositivo;
-	var contadorNegativos;
+	var contadorNegativos=0;
+	var contadorPositivos=0;
+	var contadorPares=0;
+	var contadorCeros=0;
 	var numero; 
 	var negativo=0;
 	var positivo=0;
+	var acumuladorNegativos=0;
+	var acumuladorPositivos=0;
+	var diferencia;
 	var respuesta="si";
 
-
-	while(respuesta!="no")
+	while(respuesta!="no")  //podria poner var respuesta=si y while(respuesta==true), cerrando con un confirm
 	{
 		contador++;
 		numero=prompt("Ingrese un numero");
@@ -22,30 +26,70 @@ function mostrar()
 			numero=prompt("Ingrese un numero valido");
 			numero=parseInt(numero);
 		}
+
 		if(numero<0)
 		{
 			negativo=negativo+numero;
-			promedioNegativo=negativo/contador;
-			contadorNegativos=contadorNegativos.length;
+			contadorNegativos=contadorNegativos+1; //Le sumo 1 para qeu con cada numero me sume un lugar
+			acumuladorNegativos=acumuladorNegativos+numero; //Tambien puede ir acumuladorNegativos+=numero;
+			} 
+			else 
+			{
 
-		}
-		else 
-		{
 			if(numero>0)
 			{
 				positivo=positivo+numero;
-				promedioPositivo=positivo/contador;
+				contadorPositivos=contadorPositivos+1; //Tambien puedo poner contadorPositivo++
+				acumuladorPositivos=acumuladorPositivos+numero;
+			} 
+			else  //por default es cero
+			{
+				contadorCeros=contadorCeros+1; //o contadorCeros++;
 			}
-	
+	}
+
+	if(numero%2==0) //Porque es si el numero dividido 2da cero, es par
+	{
+		contadorPares=contadorPares+1; //o contadorPares++;
 	}
 	respuesta=prompt("Desea seguir? Ingrese 'si' ");
 		}
+	
+		
+		if(contadorPositivos!=0){
+			promedioPositivo=acumuladorPositivos/contadorPositivos;
+		}
+		else
+		{
+			promedioPositivo = "No se ingresaron numeros positivos";
+		}
+		if(contadorNegativos!=0){
+			promedioNegativo=acumuladorNegativos/contadorNegativos; //El contador no puede ser 0 porque me va a dar NaN
+		} 
+		else 
+		{
+			promedioNegativo = "No se ingresaron numeros negativos";
+		}
+
+	diferencia=acumuladorPositivos-(acumuladorNegativos*(-1));
+
+
 
 	document.write("El resultado de la suma es "+negativo+" </br>");
 	document.write("El resultado de la suma es "+positivo+" </br>");
-	document.write("El promedio es </br>"+promedioNegativo);
-	document.write("El promedio de positivos es </br>"+promedioPositivo);
-	document.write("La suma de negativos y positivos es "+negativo+positivo+" </br>");
-
+	document.write("El promedio de los negativos es "+promedioNegativo+" </br>");
+	document.write("El promedio de positivos es "+promedioPositivo+" </br>");
+	document.write("La cantidad de numeros positivos es "+contadorPositivos+" </br>");
+	document.write("La cantidad de numeros negativos es "+contadorNegativos+" </br>");
+	document.write("La cantidad de ceros es "+contadorCeros+" </br>");
+	document.write("La diferencia entre los positivos y los negativos es "+diferencia+" </br>");
+	document.write("La cantidad de numeros pares es "+contadorPares+" </br>");
 
 }//FIN DE LA FUNCIÓN
+/* var respuesta=true;
+ while(respuesta==true){
+	 numero=prompt("Ingrese un numero");
+	 numero=parseInt(numero);
+		 respuesta=confirm("Desea seguir ingresando?");   Esto es porque use la variable igualada 
+		 a true y puse en el while que evalue respuesta con true
+ } */
