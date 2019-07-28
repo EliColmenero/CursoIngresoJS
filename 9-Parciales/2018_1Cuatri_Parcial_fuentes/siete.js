e6714ff6bfbfae6306f8d6c4cb565997ab462bed
eli <eli@gmail.com>
2019-07-27 23:50:55 -0300

    Ver ejercicio 7

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,28 @@
 function mostrar()
 {
+var nota;
+var sexo;
+var respuesta='si';
+var contadorNotas=0;
+var acumuladorNotas;
 
+while(respuesta=='si'){ 
+    contadorNotas++;
+    nota=prompt("Ingrese la nota");
+    nota=parseInt(nota);
+    sexo=prompt("Ingrese sexo 'f' o 'm' ");
+
+    if(nota>0 && nota<10){
+        alert("La nota es "+nota);
+    } else {
+        alert("La nota ingresada no es válida");
+    }
+    
+    if(sexo='f'){
+        alert("Es mujer");
+    } else {
+        alert("Es hombre");
+    }
+}
+respuesta=prompt("Desea seguir ingresando notas? responder 'si' o 'no' ");
 }
