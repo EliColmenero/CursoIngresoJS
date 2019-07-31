151a94e8c8245cad1ead7d842582208c4d5ff786
eli <eli@gmail.com>
2019-07-31 00:13:05 -0300

    terminar

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -4,25 +4,43 @@ var nota;
 var sexo;
 var respuesta='si';
 var contadorNotas=0;
-var acumuladorNotas;
+var acumuladorNotas=0;
+var promedio;
+var minimo=10;
+var maximo=0;
 
-while(respuesta=='si'){ 
-    
+while(respuesta=='si' && contadorNotas<5){ 
+
+        contadorNotas++;
     nota=prompt("Ingrese la nota");
     nota=parseInt(nota);
     sexo=prompt("Ingrese sexo 'f' o 'm' ");
 
-    
     if(sexo=="f" || sexo=="m"){
-        if(nota>0 && nota<10){
+        if(nota>=0 && nota<=10){
+            
             alert("La nota es "+nota+" Y el sexo es "+sexo);
-            contadorNotas++;
         } else {
-            alert("La nota ingresada no es válida");}
+            alert("La nota ingresada no es válida");
+            }
+            if(nota>maximo && (sexo=="f" || sexo=="m")){
+                maximo=nota;
+                alert("La nota es maxima es "+maximo+" y el sexo es "+sexo);
+            } 
+                if(nota<minimo && (sexo=="f" || sexo=="m")){
+                    minimo=nota;
+                    alert("La nota minima es "+minimo+" y el sexo es "+sexo); 
+                } 
+            
     } else {
         alert("El sexo no es valido");
     } 
-}
+    acumuladorNotas+=nota; 
     
-respuesta=prompt("Desea seguir ingresando notas? responder 'si' o 'no' ");
+    respuesta=prompt("Desea seguir ingresando notas? responder 'si' o 'no' ");
+}
+    promedio=acumuladorNotas/contadorNotas;    
+    alert("El promedio es "+promedio);
+
+
 }
