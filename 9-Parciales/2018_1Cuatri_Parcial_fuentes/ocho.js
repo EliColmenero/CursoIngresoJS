a705d34e9c7b874a15b5fb765319f7010a8c7ca4
alumno <alumno@utnfra.com.ar>
2019-08-01 12:11:36 -0300

    8

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -3,17 +3,34 @@ function mostrar()
 var numero;
 var letra;
 var respuesta="si";
+var contador=0;
+var cantidadPares=0;
+var cantidadImpares=0;
+var cantidadCeros=0;
 
 
 while(respuesta=='si'){ 
     numero=prompt("Ingrese un numero");
     letra=prompt("Ingrese una letra");
 
-    while(isNaN(numero) && (numero>100 && numero<=-100)){
+    while(isNaN(numero) || (numero<100 || numero>-100)){
         numero=prompt("Ingrese un numero valido");
 
     }
-    respuesta=prompt("Quiere seguir? 'si' o 'no' ");
-}
+    if(numero%2==0){
+        cantidadPares++;
+    }
+         else {
+            cantidadImpares++;
+         }
+         if(numero==0){
+            cantidadCeros++;
+         }
 
+    
+    respuesta=prompt("Quiere seguir? 'si' o 'no' ");
+} 
+    document.write("La cantidad de pares es "+cantidadPares+" <br>");
+    document.write("La cantidad de impares es "+cantidadImpares+" <br>");
+    document.write("La cantidad de ceros es "+cantidadCeros+" <br>");
 }
