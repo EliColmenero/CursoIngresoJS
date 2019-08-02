e8bdc1a52f980b1778e265d2083814d1474bd8e6
eli <eli@gmail.com>
2019-08-01 22:39:27 -0300

    ej 6 parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/dos.js
@@ -3,8 +3,6 @@ function mostrar()
 var nombre;
 var localidad;
 
-nombre=prompt("Ingrese su nombre");
-localidad=prompt("Ingrese su localidad");
 document.getElementById('laLocalidad').value=localidad;
 document.getElementById('elNombre').value=nombre;
 
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/seis.js
@@ -1,71 +1,52 @@
 function mostrar()
 {
 var hora;
+var mensaje;
 
-hora=prompt("Ingrese una hora");
-document.getElementById('laHora').value=hora;
+hora=document.getElementById('laHora').value;
+
 
 switch(hora){
-    case '6':{
-    }
-    case '7':{
-    }
-    case '8':{
-    }
-    case '9':{
-    }
-    case '10':{
-    }
+    case '6':{}
+    case '7':{}
+    case '8':{}
+    case '9':{}
+    case '10':{}
     case '11':{
         mensaje="Es de mañana";
         break;
     }
-    case '12':{
-    }
-    case '13':{
-    }
-    case '14':{
-    }
-    case '15':{
-    }
-    case '16':{
-    }
-    case '17':{
-    }
-    case '18':{
-    }
+    case '12':{}
+    case '13':{}
+    case '14':{}
+    case '15':{}
+    case '16':{}
+    case '17':{}
+    case '18':{}
     case '19':{
         mensaje="Es de tarde";
         break;
     }
-    case '20':{
-    }
-    case '21':{
-    }
-    case '22':{
-    }
-    case '23':{
-    }
-    case '1':{
-    }
-    case '2':{
-    }
-    case '3':{
-    }
-    case '4':{
-    }
-    case '5':{
-    }
+    case '1':{}
+    case '2':{}
+    case '3':{}
+    case '4':{}
+    case '5':{}
+    case '20':{}
+    case '21':{}
+    case '22':{}
+    case '23':{}
     case '24' :{
         mensaje="Es de noche";
         if(hora<24 && hora>=5){
             mensaje="A dormir";
-        } 
+                } 
+                break;
         }
     default:{
         mensaje="La hora no es válida";
         break;
     }
-    }
+}
 alert(mensaje);
 }
